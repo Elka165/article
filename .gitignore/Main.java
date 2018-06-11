5019dab0b0faaecd395a4f244c1d2c3544c8b50a
Elka165 <ela_dolna99@o2.pl>
2018-06-11 22:02:03 +0200

    cwiczenie1

---
+++ .gitignore/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner scaner=new Scanner(System.in);
+
+       Name name=new Name();
+
+       System.out.println("Ile imion chcesz wpisać:");
+       name.long1=scaner.nextInt();
+       name.longTable();
+
+       System.out.println("Wpisz imiona:");
+       name.wpiszImie();
+
+       System.out.println(name.long1);
+
+       for(String args1 : name.imie) {
+           System.out.println(args1);
+       }
+        name.maxName();
+        System.out.println("Najdłuższe imie: "  + name.imieMax);
+        System.out.println("Najkrótsze imie: " + name.imieMin);
+
+        System.out.println("Pwtarzające się imiona: " + name.mapa.keySet() + name.mapa.values() );
+    }
+
+
+}
